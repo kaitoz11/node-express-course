535c6c0580f74576dfbfe66ab150f05a4fd99d88
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2020-12-01 17:47:31 +0700

    add dynamic GET route

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get("/users",function(req,res){
         users: mockUserData
     })
 })
+app.get("/users/:id", function(req,res){
+    console.log(req.params.id);
+    res.json({
+      success: true,
+      message: "got one user",
+      users: req.params.id
+    })
+})
 app.listen(8000,function(){
     console.log("server is running");
 })
